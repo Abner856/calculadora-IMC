783bbaf049a3857bc8fba0d96c8a9c8ac6cc2948
Israel Abner <israelabnerdias@gmail.com>
2025-09-14 13:11:11 -0300

    3 commit - corrigindo erros do documento js

--- calculadora_imc.js
+++ calculadora_imc.js
@@ -7,18 +7,20 @@ function imc(){
     const altura = document.getElementById("altura");
     const peso = document.getElementById("peso");
     const resultado = document.getElementById("resultado");
-  
-    const alturaEmMetros = altura.value / 100;
-    const imc = peso.value / (alturaEmMetros * alturaEmMetros);
-    resultado.innerHTML = `Olá ${nome.value}, seu IMC é ${imc.toFixed(2)}`;
-    if (imc < 18.5) {
-        resultado.innerHTML += "<br>Classificação: Abaixo do peso";
-    } else if (imc < 25) {
-        resultado.innerHTML += "<br>Classificação: Peso normal";
-    } else if (imc < 30) {
-        resultado.innerHTML += "<br>Classificação: Sobrepeso";
-    } else {
-        resultado.innerHTML += "<br>Classificação: Obesidade";
+
+    const valorIMC = (peso.value / (altura.value * altura.value)).toFixed(2);
+
+    let classificacao = "";
+    if(valorIMC < 18.5){
+        classificacao = "abaixo do peso.";
+    } else if(valorIMC < 25){
+        classificacao = "peso normal.";
+    } else if(valorIMC < 30){
+        classificacao = "sobrepeso.";
+    } else{
+        classificacao = "obesidade.";
     }
+
+    resultado.innerHTML = `Olá ${nome.value}, seu IMC é ${valorIMC} e você está ${classificacao}`;
 }
 calcular.addEventListener("click", imc);
